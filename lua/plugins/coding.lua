11bd30109d133f544dd13be15f57f622b83bdb2d
zainmiku <fanhoufang@gmail.com>
2023-06-09 19:34:59 +0800

    cmp配置命令提示

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,8 +4,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     build = (not jit.os:find("Windows"))
-        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-      or nil,
+    	and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp" or nil,
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
@@ -18,81 +17,127 @@ return {
     },
   },
 
-  -- auto completion
-  {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is way too old
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "onsails/lspkind-nvim",
-    },
-    opts = function()
-      local cmp = require("cmp")
-      local lspkind = require('lspkind')
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-        -- Use <C-b/f> to scroll the docs
-        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        -- Use <C-k/j> to switch in items
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        -- Use <CR>(Enter) to confirm selection
-        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-        -- formatting = {
-        --   format = function(_, item)
-        --     local icons = require("lazyvim.config").icons.kinds
-        --     if icons[item.kind] then
-        --       item.kind = icons[item.kind] .. item.kind
-        --     end
-        --     return item
-        --   end,
-        -- },
-          -- 使用lspkind-nvim显示类型图标
-        formatting = {
-          format = lspkind.cmp_format({
-            with_text = true, -- do not show text alongside icons
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            before = function (entry, vim_item)
-              -- Source 显示提示来源
-              vim_item.menu = "["..string.upper(entry.source.name).."]"
-              return vim_item
-            end
-          })
-        },
+	{
+		"hrsh7th/nvim-cmp",
+		event = { "InsertEnter", "CmdlineEnter" },
+		config = function()
+			local luasnip = require("luasnip")
 
+			-- nvim-cmp setup
+			local cmp = require("cmp")
+			if cmp == nil then
+				return
+			end
+			local lspkind = require("lspkind")
 
-        experimental = {
-          ghost_text = {
-            hl_group = "LspCodeLens",
-          },
-        },
-      }
-    end,
-  },
+			cmp.setup({
+				-- show source name in menu
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						menu = {
+							buffer = "[Buffer]",
+							nvim_lsp = "[LSP]",
+							luasnip = "[LuaSnip]",
+							nvim_lua = "[Lua]",
+							dictionary = "[Dictionary]",
+							path = "[Path]",
+							cmp_tabnine = "[TabNine]",
+						},
+					}),
+				},
+
+				-- https://www.reddit.com/r/neovim/comments/t7jl7p/cmp_autocomplete_in_golang_does_not_autoselect/
+				preselect = cmp.PreselectMode.None,
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- 上一个
+					['<C-k>'] = cmp.mapping.select_prev_item(),
+					-- 下一个
+					['<C-j>'] = cmp.mapping.select_next_item(),
+					-- abort
+					["<C-c>"] = cmp.mapping.abort(),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+
+				sources = {
+					{ name = "neorg" },
+					{ name = "luasnip", priority = 100 },
+					{ name = "nvim_lsp", priority = 90 },
+					-- { name = "cmp_tabnine", priority = 98, max_item_count = 2, keyword_length = 3 },
+					{ name = "buffer", priority = 80, max_item_count = 3, keyword_length = 5 },
+					{ name = "path", priority = 80, max_item_count = 3, keyword_length = 3 },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "nvim_lua" },
+					-- disable fuzzy
+					-- { name = "dictionary", priority = 10, max_item_count = 5, keyword_length = 5 },
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline", max_item_count = 30 },
+				}),
+			})
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "luasnip", priority = 100 },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "dictionary", priority = 10, max_item_count = 5, keyword_length = 3 },
+				}),
+			})
+
+			cmp.setup.filetype("markdown", {
+				sources = cmp.config.sources({
+					{ name = "luasnip", priority = 100 },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "dictionary", priority = 10, max_item_count = 5, keyword_length = 3 },
+				}),
+			})
+		end,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+		"onsails/lspkind-nvim"
+		},
+	},
 }
